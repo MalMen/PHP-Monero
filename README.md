387b81886fbac8922680bdf7b39fe187065512fb
Jacob Briggs <jebriggsy@gmail.com>
2015-03-25 02:20:48 -0400

    Documented with PHPDoc, refactored to OOP, refactored to MySQLi

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# PHP-Monero
 PHP Integration of Monero
+
+With this simple scripts should be easy to integrate XMR payments in any PHP + MySQL/MariaDB platform.
+
+Import the xmr_payments.db file into your MySQL or MariaDB database and instantiate the PHP_Monero class
+and you are ready to use Monero on your site.
+
+The code is well documented according to PHPDoc standards and mostly follows PHP-FIG coding guidelines.
+
+Donations
+XMR: 49RPpNuDuLhayv8yHgVSNhgdvB4Uze3A9euEsBzp3groWssk2eZPEErf6LSDae9smQ78a5CfNmafYdgYnyjTEY6q4EvuPJ1
+XMR OA: pedrogaspar.pt
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Example code
+ *
+ * @filesource
+ */
+
+
+/** You need to create a MySQLi instance first */
+$mysqli = new mysqli('host','user','pass','database');
+
+/** The Monero Interface takes this MySQLi instance as an argument */
+$MoneroAPI = new \includes\PHP_Monero($mysqli);
+
+/** This will return an array with a new payment address */
+$payment_address = $MoneroAPI->create_payment_address();
+
+/** I wonder if it worked */
+if ($payment_address) {
+    echo "transfer 3 $payment_address[openalias] 5 $payment_address[payment_id]";
+    echo '<br />';
+} else {
+    echo 'Handle the error. <br />';
+}
+
+/** This will send a transfer for 5 Moneroj to the database for later processing */
+$transfer = $MoneroAPI->transfer('address', 5);	
+
+/** Did that work? *//
+if (!$transfer) {
+    echo 'Didn\'t think so <br />';
+} else {
+    echo ($transfer["amount"]/12) . " XMR will be sent to $transfer[address] <br />";
+}
+
+
+/** The below methods should be added to your Cron Jobs */
+$MoneroAPI->client_receive(); // procceing receipts
+xmr_client_transfer();	// processing transfers
+
